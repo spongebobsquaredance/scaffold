a03231ba1b45bccf712a80adb9286224cde4d80e
Carrie MacDonald <CMacDonald@genuineinteractive.com>
2016-03-07 11:38:45 -0500

    Sass and watch tasks, sample font set-up

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var del = require('del');
 var include = require('gulp-include');
+var sass = require('gulp-ruby-sass');
 
 // Clean build folder with del
 gulp.task('clean:build', function() {
@@ -14,13 +15,26 @@ gulp.task('includes', function() {
  
   gulp.src('src/includes/*.html')
     .pipe(include())
-      .on('error', console.log)
+    .on('error', console.log)
     .pipe(gulp.dest('build'));
 });
 
 // Compile Sass
+gulp.task('sass', function() {
+	return sass('src/styles/sass/screen.scss')
+	.on('error', sass.logError)
+	.pipe(gulp.dest('build/styles'));
+});
 
 // Watch task
+gulp.task('watch', function() {
+	gulp.watch('src/includes/**/*.html', ['includes']);
+	gulp.watch('src/styles/sass/**/*.scss', ['sass']);
+	// gulp.watch('src/scripts/*.js', ['js-min']);
+});
 
 // Default Gulp task
-gulp.task('default', ['clean:build', 'includes']);
+gulp.task('default', ['watch']);
+
+// Final Build task
+// gulp.task('build', ['clean:build']);
